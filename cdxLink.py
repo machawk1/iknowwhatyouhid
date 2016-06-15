5bea3b8ad3f512f72ea33dd2d7640cad38c654bd
Mat Kelly <mkelly@cs.odu.edu>
2016-06-15 15:40:27 +0000

    URI resolution.

--- cdxLink.py
+++ cdxLink.py
@@ -6,6 +6,7 @@ from surt import surt
 from multiprocessing.dummy import Pool as ThreadPool 
 import argparse
 import json
+from requests.packages.urllib3.connection import HTTPConnection
 
 # Input CSV
 #handle,tweetId,links...
@@ -35,51 +36,45 @@ if not UK:
 # Threaded fetch process that spits out URI-Ms and corresponding status codes for each 
 #  URI-R passed in within a date range
 def getMementosWorker(lineContent):
-  print lineContent
   if len(lineContent.strip()) == 0:
     return
-  print json.loads(lineContent)
-  sys.exit()
-  (handle,tweetId,links) = lineContent.split(',',2)
-  linksAry = links.split(';')
+  jData = json.loads(lineContent)
+  urls = jData['entities']['urls']
+  if len(urls) == 0:
+    return
+  url = urls[0]['expanded_url']
+  out = ''
+  #out = "Resolving " + url + "...\n"
+  ultimateURI = ''
+  try:
+      tcoResponse = requests.get(url.strip())
+      ultimateURI = tcoResponse.url
+      out += url+' '+ultimateURI+' '+str(tcoResponse.status_code)
+      
+      
+  except:
+       out += url+' --> ERROR'
+       return
   
-  if len(linksAry) == 0:
-    return; # No links found in the line
-  
-  for idx,uri in enumerate(linksAry):
-    # Expand URI for t.co's
-    if len(uri) == 0:
-     continue
 
-    print uri
-    try:
-      tcoResponse = requests.get(uri.strip())
-      uri = tcoResponse.url
-      print 'Successfully followed to '
-      print uri
-    except requests.exceptions.RequestException as e:
-      print "error"
-      print e
-      sys.exit()  
+  print out
   
-    uri = 'http://web.archive.org/cdx/search/cdx?url={0}&from={1}&to={2}'.format(uri, startDate, endDate)
-    
-    
-    #headers = {'Accept-Datetime': datetime}
-    resp = requests.get(uri)#, headers=headers)
-    cdxData = resp.content.strip().split('\n')
-  
-    buff = ''
-  
-    for idx, cdxLine in enumerate(cdxData):
-      if len(cdxLine) == 0:
-        break;
+  uri = 'http://web.archive.org/cdx/search/cdx?url={0}&from={1}&to={2}'.format(ultimateURI, startDate, endDate)
 
-      cdxFields = cdxLine.split(' ')
+  resp = requests.get(uri)
+  cdxData = resp.content.strip().split('\n')
 
-      httpStatus = cdxFields[4]
-      urim = 'http://web.archive.org/web/{0}/{1}'.format(cdxFields[1], cdxFields[2])
-      buff += "{0} {1}\n".format(urim, httpStatus)
+  buff = ''
+
+  for idx, cdxLine in enumerate(cdxData):
+    if len(cdxLine) == 0:
+      break;
+
+    cdxFields = cdxLine.split(' ')
+
+    httpStatus = cdxFields[4]
+    urim = 'http://web.archive.org/web/{0}/{1}'.format(cdxFields[1], cdxFields[2])
+    buff += "{0} {1}\n".format(urim, httpStatus)
     print buff
 
 def main():
@@ -103,14 +98,6 @@ def main():
     results = pool.map(getMementosWorker, lines)
     pool.close() 
     pool.join() 
-    
-    # OBSOLETE: less efficient than above.
-    # Query the CDX API
-    #processes = []
-    #for link in links:
-    #  process = multiprocessing.Process(target=getMementosWorker, args=(link,))
-    #  processes.append(process)
-    #  process.start()
 
 if __name__ == '__main__':
   main()
